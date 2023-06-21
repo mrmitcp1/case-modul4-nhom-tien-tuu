801c536a9cde7a563d1af87ce74724b2e39537db
Van Vu <vanvn.vu@gmail.com>
2023-06-21 13:57:00 +0700

    Added success screen

--- index.ts
+++ index.ts
@@ -5,32 +5,31 @@ import session from "express-session";
 import livereload from "connect-livereload";
 import passport from "passport";
 
-
 const PORT = 3333;
 const app = express();
-app.set('view engine','ejs');
-app.set('views','./src/views');
-const DB_URL = 'mongodb://127.0.0.1:27017/case_modul4'
-mongoose.connect(DB_URL)
-    .then(() => console.log('DB Connected!'))
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+const DB_URL = "mongodb://127.0.0.1:27017/case_modul4";
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("DB Connected!"))
 
-    .catch(error => console.log('DB connection error:', error.message));
+  .catch((error) => console.log("DB connection error:", error.message));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(session({
-    secret: 'keyboard cat',
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
-}));
+    cookie: { secure: false },
+  })
+);
 app.use(livereload());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.listen(PORT, () => {
-
-    console.log("App running on port: " + PORT)
-
-})
+  console.log("App running on port: " + PORT);
+});
