eb9023b0d738ca28423b8020827ab9c5439b9b92
mrmitcp1 <mrmitcp55@gmail.com>
2023-06-21 11:24:38 +0700

    create-form-case

---
+++ dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const express_session_1 = __importDefault(require("express-session"));
+const connect_livereload_1 = __importDefault(require("connect-livereload"));
+const passport_1 = __importDefault(require("passport"));
+const PORT = 3333;
+const app = (0, express_1.default)();
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+const DB_URL = 'mongodb://127.0.0.1:27017/case_modul4';
+mongoose_1.default.connect(DB_URL)
+    .then(() => console.log('DB Connected!'))
+    .catch(error => console.log('DB connection error:', error.message));
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use((0, express_session_1.default)({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+app.use((0, connect_livereload_1.default)());
+app.use(passport_1.default.initialize());
+app.use(passport_1.default.session());
+app.listen(PORT, () => {
+    console.log("App running on port: " + PORT);
+});
+//# sourceMappingURL=index.js.map
