4aa2891c7ffa0d302d93a58598620fa2b6980984
Van Vu <vanvn.vu@gmail.com>
2023-06-22 10:10:21 +0700

    pull from db fix

--- src/schemas/pickuplocation.schema.ts
+++
@@ -1,6 +0,0 @@
-import { Schema, model } from "mongoose";
-const pickupLocaltionSchema = new Schema({
-    pickupLocation_name: String,
-    pickup_time: Date
-});
-export const PickupLocaltion = model('PickupLocaltions',pickupLocaltionSchema)
