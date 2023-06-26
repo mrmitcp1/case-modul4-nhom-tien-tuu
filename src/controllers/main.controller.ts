56f78c442a695cc6ff105192f4d457ca9cdee546
CaoMinhThuan <caominhthuan2506@gmail.com>
2023-06-26 23:36:09 +0700

    add maincontroller with sigin gg

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -1,10 +1,20 @@
+import { User } from "../schemas/user.schema";
 export class MainController {
-  static showHomePage(req: any, res: any) {
+  static async showHomePage(req: any, res: any) {
     let role;
     let user;
-    if (req.user) {
+    if (req.user.username) {
       user = req.user;
       role = req.user.role;
+    } else {
+      let userInfo = await User.findOne({ _id: req.user.id })
+      user = {
+        id: userInfo._id,
+        username: userInfo.user_name,
+        role : userInfo.user_role
+       }
+      role = userInfo.user_role;
+      
     }
     res.render("index", { userState: role, userGreet: user });
   }
