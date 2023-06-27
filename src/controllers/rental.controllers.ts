c311bce890823b228028c222e72abefff892e535
HieuT23511 <hieut23511@gmail.com>
2023-06-27 11:46:32 +0700

    logic date pickup

--- src/controllers/rental.controllers.ts
+++ src/controllers/rental.controllers.ts
@@ -4,6 +4,7 @@ import {Car} from "../schemas/car.schema";
 import {DropofLocaltion} from "../schemas/dropoflocaltion.schema"
 import {RentalDetail} from "../schemas/rentaldetail.schema"
 import {PickupLocaltion} from "../schemas/pickuplocaltion.schema";
+import {User} from "../schemas/user.schema";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -32,17 +33,18 @@ class RentalControllers {
             let {nameCarSelect, dropofLocation, datePickup, dateDropof} = req.body;
             const pickupDate: Date = new Date(datePickup);
             const dropoffDate: Date = new Date(dateDropof);
-            const today: Date = new Date()
             const dataCar = await Car.findOne({_id: req.params.id}).populate({
                 path: "pickup",
                 select: "pickupLocaltion_name",
             });
                 // @ts-ignore
-                const numberOfDays: number = ((dropoffDate - pickupDate) / (1000 * 60 * 60 * 24));
+                const numberOfDays: number = Math.ceil((dropoffDate - pickupDate) / (1000 * 60 * 60 * 24));
                 let totalCost = numberOfDays * dataCar.car_rentalPrice;
                 const dataPickupLocation = await PickupLocaltion.findOne({_id: dataCar.pickup._id})
                 const dataDropLocation = await DropofLocaltion.findOne({_id: dataCar.drop._id})
                 let newRentalDetail = await new RentalDetail({
+                    car_id: dataCar._id,
+                    user_id: req.user.id,
                     datePickup: datePickup,
                     dateDrop: dateDropof,
                     total_cost: totalCost,
@@ -55,7 +57,8 @@ class RentalControllers {
                 const p1 = dataCar.save();
                 const p2 = dataDropLocation.save();
                 const p3 = dataPickupLocation.save();
-                let [dataCars, dataDropLocations, dataPickupLocations] = await Promise.all([p1, p2, p3])
+                const p4 = newRentalDetail.save();
+                let [dataCars, dataDropLocations, dataPickupLocations,rentalDetail] = await Promise.all([p1, p2, p3, p4])
                 res.render("bookingOrderDetail", {
                     car: dataCars,
                     pickupLocation: dataPickupLocations,
