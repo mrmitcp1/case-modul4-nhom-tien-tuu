f3cb9cfb3708e6e320f54b8517e7399680d3799b
CaoMinhThuan <caominhthuan2506@gmail.com>
2023-06-27 11:47:42 +0700

    fix main

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -1,31 +1,33 @@
-import { User } from "../schemas/user.schema";
+import {User} from "../schemas/user.schema";
+
 export class MainController {
-  static async showHomePage(req: any, res: any) {
-    let role;
-    let user;
-    if(req.user) {
-    if (req.user.username) {
-      user = req.user;
-      role = req.user.role;
-    } else {
-      let userInfo = await User.findOne({ _id: req.user.id })
-      user = {
-        id: userInfo._id,
-        username: userInfo.user_name,
-        role : userInfo.user_role
-       }
-      role = userInfo.user_role;
-      
+    static async showHomePage(req: any, res: any) {
+        let role;
+        let user;
+        if (req.user) {
+            if (req.user.username) {
+                user = req.user;
+                role = req.user.role;
+            } else {
+                let userInfo = await User.findOne({_id: req.user.id})
+                user = {
+                    id: userInfo._id,
+                    username: userInfo.user_name,
+                    role: userInfo.user_role
+                }
+                role = userInfo.user_role;
+            }
+            res.render("index", {userState: role, userGreet: user});
+        }
     }
-    res.render("index", { userState: role, userGreet: user });
-  }}
-  static getInfoUser(req,res){
-    let role;
-    let user;
-    if (req.user) {
-      user = req.user;
-      role = req.user.role;
+
+    static getInfoUser(req, res) {
+        let role;
+        let user;
+        if (req.user) {
+            user = req.user;
+            role = req.user.role;
+        }
+        return user.username
     }
-    return user.username
-  }
 }
